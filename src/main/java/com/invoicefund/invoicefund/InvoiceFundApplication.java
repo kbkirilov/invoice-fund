0cd9824d85a343454de6f89a2095c7f9a38a2cc2
kbkirilov <kirilovmail90@gmail.com>
2025-04-15 10:51:01 +0300

    Initial commit

---
+++ src/main/java/com/invoicefund/invoicefund/InvoiceFundApplication.java
@@ -0,0 +1,13 @@
+package com.invoicefund.invoicefund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceFundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvoiceFundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/invoicefund/invoicefund/InvoiceFundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invoicefund.invoicefund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceFundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
