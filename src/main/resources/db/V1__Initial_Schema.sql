02919b8f50985f3b8f9ed7f24a8456c6b9473baf
kbkirilov <kirilovmail90@gmail.com>
2025-04-15 15:40:50 +0300

    db - Improved relationships and arranged the columns in the tables better

--- src/main/resources/db/V1__Initial_Schema.sql
+++ src/main/resources/db/V1__Initial_Schema.sql
@@ -19,8 +19,8 @@ create table users
     last_name varchar(20) not null ,
     email varchar(50) not null ,
     address varchar(255) not null ,
-    role_id int not null ,
     registered_at datetime default  current_timestamp ,
+    role_id int not null ,
 
     constraint fk_users_role_id
         foreign key (role_id) references roles (id)
@@ -28,25 +28,28 @@ create table users
 
 create table admins
 (
+    id int auto_increment primary key ,
     user_id int ,
 
     constraint fk_admins_user_id
         foreign key (user_id) references users (id)
 );
 
-create table businesses
+create table vendors
 (
-    user_id int ,
+    id int auto_increment primary key ,
     company_name varchar(255) ,
+    user_id int ,
 
-    constraint fk_businesses_user_id
+    constraint fk_vendors_user_id
         foreign key (user_id) references users (id)
 );
 
 create table investors
 (
-    user_id int ,
+    id int auto_increment primary key ,
     company_name varchar(255) ,
+    user_id int ,
 
     constraint fk_investors_user_id
         foreign key (user_id) references users (id)
@@ -60,40 +63,43 @@ create table debtors
     email varchar(50) not null
 );
 
-create table invoices
-(
-    id int auto_increment primary key ,
-    issuer_id int ,
-    debtor_id int ,
-    amount decimal(15,2) not null ,
-    description varchar(500) not null ,
-    created_at datetime default current_timestamp ,
-    issue_date datetime not null ,
-    due_date datetime not null ,
-    status_id int default 1 ,
-
-    constraint fk_invoices_users_id
-        foreign key (issuer_id) references businesses (user_id) ,
-    constraint fk_invoices_debtor_id
-        foreign key (debtor_id) references debtors (id) ,
-    constraint fk_invoices_status_id
-        foreign key (status_id) references statuses (id)
-);
-
 create table auctions
 (
     id int auto_increment primary key ,
-    user_id int ,
-    status_id int default 1 ,
     start_date datetime not null ,
     end_date datetime not null ,
     bid_step decimal(15,2) not null ,
     created_at datetime default current_timestamp,
     updated_ad datetime ,
+    user_id int ,
+    status_id int default 1 ,
 
     constraint fk_auctions_user_id
-        foreign key (user_id) references businesses (user_id) ,
+        foreign key (user_id) references users (id) ,
     constraint fk_auctions_status_id
+        foreign key (status_id) references statuses (id)
+);
+
+create table invoices
+(
+    id int auto_increment primary key ,
+    amount decimal(15,2) not null ,
+    description varchar(500) not null ,
+    created_at datetime default current_timestamp ,
+    issue_date datetime not null ,
+    due_date datetime not null ,
+    user_id int ,
+    debtor_id int ,
+    auction_id int ,
+    status_id int default 1 ,
+
+    constraint fk_invoices_users_id
+        foreign key (user_id) references users (id) ,
+    constraint fk_invoices_debtor_id
+        foreign key (debtor_id) references debtors (id) ,
+    constraint fk_invoices_auction_id
+        foreign key (auction_id) references auctions (id) ,
+    constraint fk_invoices_status_id
         foreign key (status_id) references statuses (id)
 );
 
@@ -102,9 +108,12 @@ create table bids
     id int auto_increment primary key ,
     amount decimal(15,2) not null ,
     user_id int ,
+    auction_id int,
 
     constraint fk_bids_user_id
-        foreign key (user_id) references investors (user_id)
+        foreign key (user_id) references users (id) ,
+    constraint fk_bids_auction_id
+        foreign key (auction_id) references auctions (id)
 );
 
 
--- src/main/resources/db/V1__Seed_Initial_data.sql
+++ src/main/resources/db/V1__Seed_Initial_data.sql
@@ -21,30 +21,31 @@ values
     (3, 'jeffbezos', 'password', 'Jeff', 'Bezos',
      'jbefoz@amazon.com', 'San Fransisco, SF112, USA', 3);
 
-insert into admins (user_id)
+insert into admins (id, user_id)
 values
-    (1);
+    (1, 1);
 
-insert into businesses (user_id, company_name)
+insert into vendors (id, company_name, user_id)
 values
-    (2, 'Lab Solutions');
+    (1, 'Lab Solutions', 2);
 
-insert into businesses (user_id, company_name)
+insert into investors (id, company_name, user_id)
 values
-    (3, 'Amazon');
+    (1, 'Amazon', 3);
 
 insert into debtors (id, company_name, address, email)
 values
     (1, 'BuzziSpace', 'Antwerp, Belgium', 'hello@buzzispace.com');
 
-insert into invoices (id, issuer_id, debtor_id, amount, description, issue_date, due_date)
-values
-    (1, 2, 1, 10000.00, 'Marketing images for a new product.',
-     '2025-04-15', '2025-06-15');
-
 insert into auctions (id, user_id, start_date, end_date, bid_step)
 values
     (1, 2, '2025-04-20', '2025-04-25', 250.00);
+
+insert into invoices (id, amount, description, issue_date, due_date, user_id, debtor_id, auction_id)
+values
+    (1, 10000.00, 'Marketing images for a new product.',
+     '2025-04-15', '2025-06-15', 2, 1, 1);
+
 
 
 
